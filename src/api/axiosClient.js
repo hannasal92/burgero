88b8c086274380db28561c320c5b030d56be2f94
Hanna Salameh <hannasalameh9@gmail.com>
2025-12-24 14:10:32 +0200

    take the backend url from env

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAuthContext } from "../context/AuthContext";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:3000/api",
   headers: { "Content-Type": "application/json" },
 });
 
